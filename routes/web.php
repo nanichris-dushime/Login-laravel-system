84600f762a8573987dfbb9070eff25263f4268b5
nanichris-dushime <dushimenani007@gmail.com>
2026-05-25 15:36:29 +0200

    webphp updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 // Show the Login Form
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
@@ -13,6 +14,10 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::get('/dashboard', function () {
     return 'Welcome to your secure dashboard!';
 })->middleware('auth');
+
+// Register Routes
+Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
 // Logout Route
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
